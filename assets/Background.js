3d1b1c58aa3c9c4b1cd53ee4f1a93e478d43574b
ilaydacaan <95634263+ilaydacaan@users.noreply.github.com>
2024-04-28 23:33:26 +0300

    Giriş yap ve kayıt ol kısımları eklendi. Firebase bağlantısı yapıldı.

--- assets/Background.js
+++ assets/Background.js
@@ -4,7 +4,7 @@ import {View, ImageBackground} from 'react-native';
 const Background = ({ children }) => {
   return (
     <View>
-      <ImageBackground source={require("C:/GurmeRota/src/images/arkaplan1.jpg")} style={{ height: '100%' }} />
+      <ImageBackground source={require("C:/GurmeRota - Kopya/src/images/arkaplan1.jpg")} style={{ height: '100%' }} />
       <View style={{ position: "absolute" }}>
         {children}
       </View>
--- src/screens/GirisYap.js
+++ src/screens/GirisYap.js
@@ -1,10 +1,27 @@
-import {View, Text, Touchable, TouchableOpacity} from 'react-native'
+import {View, KeyboardAvoidingView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity} from 'react-native'
+import { useNavigation } from '@react-navigation/core'
+import React, { useEffect, useState } from 'react'
 import React from 'react'
 import Background from '../../assets/Background'
 import Btn from '../../assets/Btn'
 import Field from '../../assets/Field'
+import {auth} from '../../firebase'
 
 const GirisYap = (props) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigation = useNavigation()
+
+  const handleSignIn = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      Alert.alert('Başarılı', 'Giriş Başarılı');
+    } catch (error) {
+      Alert.alert('Hata', error.message);
+    }
+  };
+
+
   return (
     <Background>
     <View style={{alignItems: 'center', width: 460}}>
@@ -35,16 +52,19 @@ const GirisYap = (props) => {
         
         <Field
           placeholder="Email"
+          value={email} 
+          onChangeText={text => setEmail(text)}
           keyboardType={'email-address'}
         />
-        <Field placeholder="Şifrenizi giriniz" secureTextEntry={true} />
+        <Field placeholder="Şifrenizi giriniz" value={password}
+          onChangeText={text => setPassword(text)} secureTextEntry={true} />
         <View
           style={{alignItems: 'flex-end', width: '65%', paddingRight: 18, marginTop:10, marginBottom: 30}}>
           <Text style={{color: '#006A42', fontWeight: 'bold', fontSize: 16,right:23}}>
             Şifremi Unuttum
           </Text>
         </View >
-        <Btn textColor='white' bgColor={'#006A42'} btnLabel="Giriş Yap" Press={() => alert("Giriş Başarılı")} />
+        <Btn textColor='white'   bgColor={'#006A42'} btnLabel="Giriş Yap" Press={() => alert("Giriş Başarılı")} />
         <View style={{ display: 'flex', flexDirection :'row', justifyContent: "center" }}>
           <Text style={{ fontSize: 16, fontWeight:"bold",alignItems:'center' }}>Hesabın yok mı? </Text>
           <TouchableOpacity onPress={() => props.navigation.navigate("GirisYap")}>
